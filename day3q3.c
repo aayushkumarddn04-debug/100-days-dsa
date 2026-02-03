d9d05cc779095b0d6558ade1dac77b0aae137f99
aayushkumarddn04-debug <aayushkumar.ddn04@gmail.com>
2026-02-03 22:49:34 +0530

    commit

---
+++ day3q3.c
@@ -0,0 +1,43 @@
+//Problem: Implement linear search to find key k in an array. Count and display the number of comparisons performed.
+
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    
+    // Read size of array
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    // Read array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Read key to search
+    scanf("%d", &k);
+    
+    int comparisons = 0;
+    int foundIndex = -1;
+    
+    // Linear search
+    for (int i = 0; i < n; i++) {
+        comparisons++;
+        if (arr[i] == k) {
+            foundIndex = i;
+            break;
+        }
+    }
+    
+    // Output result
+    if (foundIndex != -1) {
+        printf("Found at index %d\n", foundIndex);
+    } else {
+        printf("Not Found\n");
+    }
+    
+    printf("Comparisons = %d\n", comparisons);
+    
+    return 0;
+}
